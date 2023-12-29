673d31fe571c1b75626ad16f77777d9e4a49860b
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2023-12-29 03:59:30 +0100

    planets

--- src/main.ts
+++ src/main.ts
@@ -3,50 +3,84 @@ import * as THREE from 'three'
 var planet: string = "earth";
 
 var sunbtn = document.querySelector(".sun")
-var earthbtn = document.querySelector(".earth")
+var mercurybtn = document.querySelector(".mercury")
 var venusbtn = document.querySelector(".venus")
+var earthbtn = document.querySelector(".earth")
+var moonbtn = document.querySelector(".moon")
+var marsbtn = document.querySelector(".mars")
+var jupiterbtn = document.querySelector(".jupiter")
+var saturnbtn = document.querySelector(".saturn")
+var uranusbtn = document.querySelector(".uranus")
+var neptunebtn = document.querySelector(".neptune")
 
-function toSun() {
-  planet = "sun"
-  cube.updateGravity()
-}
+var dinosaur = document.querySelector(".dinosaur") as HTMLElement
+var velocity = 0;
+window.addEventListener("keydown", function(event) {
+  if (event.code === "Space") {
+    velocity = +20;
+  }
+});
 
-function toEarth() {
-  planet = "earth"
-  cube.updateGravity()
-}
-
-function toVenus() {
-  planet = "venus"
-  cube.updateGravity()
+function toPlanet(planetName: string) {
+  planet = planetName
+  //cylinder.updateGravity()
 }
 
 if (sunbtn) {
-  sunbtn.addEventListener("click", toSun)
+  sunbtn.addEventListener("click", function() { toPlanet('sun'); });
 }
 
-if (earthbtn) {
-  earthbtn.addEventListener("click", toEarth)
+if (mercurybtn) {
+  mercurybtn.addEventListener("click", function() { toPlanet("mercury"); });
 }
 
 if (venusbtn) {
-  venusbtn.addEventListener("click", toVenus)
+  venusbtn.addEventListener("click", function() { toPlanet("venus"); });
 }
 
-var planets:string[] = ["sun", "mercury", "venus", "earth", 
-"mars", "jupiter", "saturne", "uranus", "neptune"]
+if (earthbtn) {
+  earthbtn.addEventListener("click", function() { toPlanet("earth"); });
+}
 
-var sunRadius: number = 10
-var sunGravity: number = 27.4
-var sunRadius: number = 15
-var earthGravity: number = 0.9
-var earthRadius: number = 10
-var jupiterGravity: number = 2.479
-var saturnGravity: number = 1.044
-var marsGravity: number = 0.371
-var venusGravity: number = 0.887 // Uranus too
-var neptuneGravity: number = 1.11
-var mercuryGravity: number = 0.37
+if (moonbtn) {
+  moonbtn.addEventListener("click", function() { toPlanet("moon"); });
+}
+
+if (marsbtn) {
+  marsbtn.addEventListener("click", function() { toPlanet("mars"); });
+}
+
+if (jupiterbtn) {
+  jupiterbtn.addEventListener("click", function() { toPlanet("jupiter"); });
+}
+
+if (saturnbtn) {
+  saturnbtn.addEventListener("click", function() { toPlanet("saturn"); });
+}
+
+if (uranusbtn) {
+  uranusbtn.addEventListener("click", function() { toPlanet("uranus"); });
+}
+
+if (neptunebtn) {
+  neptunebtn.addEventListener("click", function() { toPlanet("neptune"); });
+}
+
+var gravity: Record<string, number> = {
+  "sun": 274,
+  "earth": 9.807,
+  "moon": 1.62,
+  "mercury": 3.7,
+  "venus": 8.87,
+  "uranus": 8.87,
+  "mars": 3.71,
+  "jupiter": 24.79,
+  "saturn": 10.44,
+  "neptune": 11.15
+}
+
+var sunRadius: number = 25
+var earthRadius: number = 20
 
 const width = window.innerWidth
 const height = window.innerHeight
@@ -62,88 +96,28 @@ mainCamera.position.z = 50
 
 const scene = new THREE.Scene()
 
-class Cube {
-  position: THREE.Vector3
-  velocity: {x: number, y: number}
-  gravity: number
-  cube: THREE.Mesh
-
-  constructor(gravity: number, position: {x: number, y: number, z: number}) {
-    this.position = new THREE.Vector3(position.x, position.y, position.z)
-    this.velocity = {
-      x: 0,
-      y: 0
-    }
-    this.gravity = gravity
-    this.cube = this.draw()
-  }
-
-  draw() {
-    const geometry = new THREE.BoxGeometry(3, 3, 5); 
-    const material = new THREE.MeshBasicMaterial( {color: "beige"} ); 
-    const cube = new THREE.Mesh( geometry, material ); 
-    cube.position.set(this.position.x, this.position.y, this.position.z)
-    cube.renderOrder = 999
-    scene.add(cube);
-    return cube
-  }
-
-  update() {
-    console.log(this.gravity)
-    this.position.y += this.velocity.y;
-    if (this.position.y > radiusF(planet)) {
-      this.velocity.y -= this.gravity;
-    } else if (this.position.y < radiusF(planet)) {
-      this.position.y = radiusF(planet);
-    }
-    this.cube.position.set(this.position.x, this.position.y-10, this.position.z)
-  }
-
-  updateGravity() {
-    this.gravity = gravityF(planet)
-  }
-}
-
-function gravityF(planet: string) {
-  switch (planet) {
-    case "sun":
-      return sunGravity
-    case "earth":
-      return earthGravity
-    case "venus":
-      return venusGravity
-    default:
-      return 1
-  }
-}
-
-function radiusF(planet: string) {
-  switch (planet) {
-    case "sun":
-      return sunRadius
-    case "earth":
-      return earthRadius
-    default:
-      return earthRadius
-  }
-}
-
-var cube = new Cube(gravityF(planet), {x: 0, y: radiusF(planet), z: 15})
-
 class Planet {
   geometry: any
   material: any
   element: any
+  radius: number
 
-  constructor(geometry: any, material: any) {
-    this.geometry = geometry
-    this.material = material
+  constructor(radius: number, texturePath: string) {
+    this.radius = radius
+    this.geometry = new THREE.SphereGeometry(radius, 40, 30)
+
+    const textureLoader = new THREE.TextureLoader();
+    const texture = textureLoader.load(texturePath); 
+
+    this.material = new THREE.MeshStandardMaterial({ map: texture });
+
     this.element = new THREE.Mesh(this.geometry, this.material)
   }
 
   draw() {
     scene.add(this.element)
-    this.element.position.y = -10
+    this.element.position.x = -20
+    this.element.position.y = 0
     this.element.rotation.x += 0.01
     this.element.rotation.y += 0.01
   }
@@ -151,59 +125,73 @@ class Planet {
   remove() {
     scene.remove(this.element);
   }
-
 }
 
-let sun = new Planet(new THREE.SphereGeometry(sunRadius, 40, 30), new THREE.MeshPhysicalMaterial({ color: '#FDB813'}))
-let earth = new Planet(new THREE.SphereGeometry(10, 40, 20), new THREE.MeshPhysicalMaterial({ color: 'green'}))
-let venus = new Planet(new THREE.SphereGeometry(10, 40, 20), new THREE.MeshPhysicalMaterial({ color: '#8A420A'}))
+let textured: Record<string, string> = {
+  "sun": "https://i.imgur.com/NT8Vd4p.jpg",
+  "mercury": "https://i.imgur.com/UQQRIZg.jpg",
+  "venus": "https://i.imgur.com/Wa8lpF6.jpg",
+  "earth": "https://i.imgur.com/Tajyxwl.jpg",
+  "moon": "https://i.imgur.com/OjvY5Pv.jpg",
+  "mars": "https://i.imgur.com/U2QZveA.jpg",
+  "jupiter": "https://i.imgur.com/ZUVwSv5.jpg",
+  "saturn": "https://i.imgur.com/ITtPqGy.jpg",
+  "uranus": "https://i.imgur.com/wj611Q1.jpg",
+  "neptune": "https://i.imgur.com/KU2X0rf.jpg"
+}
+
+let sun = new Planet(sunRadius, textured["sun"]);
+let mercury = new Planet(earthRadius, textured["mercury"])
+let earth = new Planet(earthRadius, textured["earth"])
+let moon = new Planet(earthRadius - 5, textured["moon"])
+let venus = new Planet(earthRadius, textured["venus"])
+let mars = new Planet(earthRadius, textured["mars"])
+let jupiter = new Planet(sunRadius, textured["jupiter"])
+let saturn = new Planet(sunRadius, textured["saturn"])
+let uranus = new Planet(earthRadius, textured["uranus"])
+let neptune = new Planet(earthRadius, textured["neptune"])
+
+let planets:Record<string, Planet> = {
+  "sun": sun,
+  "mercury": mercury,
+  "venus": venus,
+  "earth": earth,
+  "moon": moon,
+  "mars": mars,
+  "jupiter": jupiter,
+  "saturn": saturn,
+  "uranus": uranus,
+  "neptune": neptune
+};
 
 const light = new THREE.DirectionalLight(0xFFFFFF, 1)
-light.position.set(0, 6, 4)
+light.position.set(2, 1, 4)
 scene.add(light)
 
-const lighttop = new THREE.DirectionalLight(0xFFFFFF, 5)
-lighttop.position.set(1, 2, 4)
-scene.add(lighttop)
-
-function dots(n: number): THREE.Mesh<THREE.SphereGeometry, THREE.MeshBasicMaterial, THREE.Object3DEventMap> [] {
-  let arr: THREE.Mesh<THREE.SphereGeometry, THREE.MeshBasicMaterial, THREE.Object3DEventMap>[] = []
-  for (let i = 0; i < n; i++) {
-    let dotGeometry = new THREE.SphereGeometry(Math.random(), 12, 12);
-    let dotMaterial = new THREE.MeshBasicMaterial({color: "orange"}); // Change the color as needed
-    let dot = new THREE.Mesh(dotGeometry, dotMaterial);
-    let spherical = new THREE.Spherical(
-      sunRadius, // radius
-      2 * Math.PI * Math.random(), // polar angle
-      Math.acos(2 * Math.random() - 1) // azimuthal angle
-    );
-    dot.position.setFromSpherical(spherical);
-    arr.push(dot)
-  }
-  return arr;
-}
-
-let manyDots = dots(100)
-
-for (var adot of manyDots) {
-  sun.element.add(adot)
-}
+const geometry = new THREE.CircleGeometry( 5, 32 ); 
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load("texturePath"); 
+const material = new THREE.MeshStandardMaterial({ map: texture });
+const circle = new THREE.Mesh(geometry, material); 
+scene.add(circle)
 
 var currentPlanet: Planet;
 
 function animate() {
   requestAnimationFrame(animate)
-  cube.update()
-
+  //cylinder.update()
+  velocity -= (gravity[planet] / 7);
+  if (dinosaur) {
+    dinosaur.style.bottom = (parseFloat(dinosaur.style.bottom) || 0) + velocity + "px";
+    if (dinosaur.style.bottom < 0 + "px") {
+      dinosaur.style.bottom = 10 + "px";
+    }
+  }
+    
   if (currentPlanet)
     currentPlanet.remove()
-  
-  if (planet == "earth")
-    currentPlanet = earth
-  else if (planet == "sun")
-    currentPlanet = sun
-  else if (planet == "venus")
-    currentPlanet = venus
+
+  currentPlanet = planets[planet];
     
   if (currentPlanet)
     currentPlanet.draw()
@@ -212,29 +200,3 @@ function animate() {
 }
 
 animate()
-
-function control(e: KeyboardEvent) {
-  switch(e.key) {
-    case " ":
-      cube.velocity.y = 5
-      console.log(cube.position.y)
-      break;
-    case "ArrowRight":
-      cube.velocity.x = 2
-      break
-  }
-}
-
-function down(e: KeyboardEvent) {
-  switch(e.key) {
-    case " ":
-      cube.velocity.y = 0
-      break;
-    case "ArrowRight":
-      cube.velocity.x = 0
-      break
-  }
-}
-
-window.addEventListener("keydown", control)
-window.addEventListener("keyup", down)
